d7507d2530aecfcf00849135d6e4cf1a4ebbad8a
Tomasz Topola <t.topola.yt@gmail.com>
2021-03-02 20:20:15 +0100

    add testBoardGenerator method

--- src/me/tomasztopola/Main.java
+++ src/me/tomasztopola/Main.java
@@ -3,6 +3,7 @@ package me.tomasztopola;
 import me.tomasztopola.clients.tomasztopola.NewHope;
 import me.tomasztopola.competition.Contest;
 import me.tomasztopola.competition.OneVsOne;
+import me.tomasztopola.utils.BoardGenerator;
 
 public class Main {
 
@@ -19,7 +20,15 @@ public class Main {
     private static void oneVsOne(){
         OneVsOne competition;
         competition = new OneVsOne(new NewHope(17), new NewHope(17));
-
         competition.runBattle();
+    }
+
+    private static void testBoardGenerator(){
+        BoardGenerator board = new BoardGenerator();
+        for(int i=0; i<100; i++){
+            System.out.println("Generated board No. " + i);
+            board.generate();
+            board.print();
+        }
     }
 }
