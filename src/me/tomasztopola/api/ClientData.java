1cdf8f432bd4cfd8784da8d56a697363a515f193
Tomasz Topoła <66322083+TomaszTopola@users.noreply.github.com>
2021-03-15 19:28:39 +0100

    assert multiple attacks

--- src/me/tomasztopola/api/ClientData.java
+++ src/me/tomasztopola/api/ClientData.java
@@ -6,7 +6,9 @@ import me.tomasztopola.utils.Location;
  * provides getFloatingShips to opponent
  */
 public class ClientData {
-    BattleShipClient client;
+    private BattleShipClient client;
+    private boolean hasntAttacked = true;
+
     public ClientData(BattleShipClient client){
         this.client = client;
     }
@@ -14,7 +16,10 @@ public class ClientData {
     public int getFloatingShips(){
         return client.getFloatingShips();
     }
+
     public void attack(Location target){
+        assert hasntAttacked : "Multiple attack called. You can send coordinates only once in round.";
         client.receiveAttack(target);
+        hasntAttacked = false;
     }
 }
